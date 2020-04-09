f76f80048b42b5db7e2af92c9fc9c7ac83333336
Pranay Agarwal <pranayag15@gmail.com>
2020-04-09 12:59:29 +0530

    email modifications

--- src/component/Portfolio/Contact.jsx
+++ src/component/Portfolio/Contact.jsx
@@ -8,7 +8,7 @@ import "../../style2/contact.css";
 class ContactForm extends Component {
   onFinish = (values) => {
     console.log('Sending mail...', window.location.href, window.location.hostname)
-    axios.post('/sendmail', values)
+    axios.post('http://localhost:8000/sendmail', values)
     .then(res =>{
       console.log(res, "response")
       message.success('Email sent successfully');
